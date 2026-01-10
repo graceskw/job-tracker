81b88f49dc9dea9a28279b097bed7ba1d4f3cc8b
So Ki Wai Grace <so.kwgrace@gmail.com>
2026-01-11 00:41:54 +0800

    [WIP] feat: create job status enum

---
+++ backend/src/main/java/dev/graceskw/backend/enums/ApplicationStatus.java
@@ -0,0 +1,14 @@
+package dev.graceskw.backend.enums;
+
+public enum ApplicationStatus {
+    INTERESTED,
+    APPLIED,
+    DO_ONLINE_TEST,
+    COMPLETED_ONLINE_TEST,
+    // WAITING,
+    INTERVIEW_SCHEDULED,
+    COMPLETED_INTERVIEW,
+    OFFERED,
+    REJECTED,
+    WITHDRAWN
+}
