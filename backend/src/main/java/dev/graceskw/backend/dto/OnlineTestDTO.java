9bd8028dc31b346f7648c2dcf05ce70a1597aa38
So Ki Wai Grace <so.kwgrace@gmail.com>
2026-01-11 18:46:46 +0800

    create OnlineTestDTO.java

---
+++ backend/src/main/java/dev/graceskw/backend/dto/OnlineTestDTO.java
@@ -0,0 +1,21 @@
+package dev.graceskw.backend.dto;
+
+import java.util.List;
+
+import dev.graceskw.backend.enums.OnlineTestType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@AllArgsConstructor
+@Builder
+@Data
+public class OnlineTestDTO {
+    private Long jobId;
+    private String dateReceived;
+    private String deadline;
+    private List<OnlineTestType> onlineTestTypes;
+
+    public OnlineTestDTO() {
+    }
+}
